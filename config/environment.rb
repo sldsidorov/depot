64ecb4ab05cf03b8fb4b81c8ffbf776c640bc22a
sldsidorov <itdsidorov@outlook.com>
2015-03-16 17:16:54 +0300

    Integration Test

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      authentication: 'itdsidorov@gmail.com',
+      password: 'c7_vm_je_e1',
+      enable_starttls_auto: true
+  }
+end
